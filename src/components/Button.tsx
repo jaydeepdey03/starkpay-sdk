c78efef8d9e82b1f3c017bb139969084392170c2
Jaydeep Dey <jaydeep.dey03@gmail.com>
2025-01-03 14:04:45 +0530

    more bugs

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,6 +2,8 @@ import React from "react";
 
 export default function Button() {
   return (
-    <button className="outline bg-blue-500 text-white">Hello World</button>
+    <button className="outline bg-blue-500 text-white px-3 py-2 rounded">
+      Hello World
+    </button>
   );
 }
